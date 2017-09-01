413c5b6b7d472f6edb86955df354039a9bb49318
yujishi <yujishi@higohappy.com>
2017-09-01 14:54:39 +0800

    MOD:增加一些组合特征

--- t_adult/train.py
+++ t_adult/train.py
@@ -57,16 +57,52 @@ def feature_extract(source):
 
     combine_feature = np.empty((data.shape[0], 1), dtype=np.int8)
     ttmp = np.empty((data.shape[0], 1), dtype=np.int8)
-    #+0.02education_native-country
+    #+0.002education_native-country
     combine_feature = np.core.defchararray.add(data[:,3].astype(np.str), "#")
     combine_feature = np.core.defchararray.add(combine_feature, data[:,13])
     combine_feature = combine_feature.reshape((combine_feature.shape[0], 1))
     #+0.3age_occupation 
     ttmp = np.core.defchararray.add(data[:,0].astype(np.str), "#")
     combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,12]).reshape((combine_feature.shape[0], 1))))
+    #-race_sex
+    #ttmp = np.core.defchararray.add(data[:,8].astype(np.str), "#")
+    #combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,9]).reshape((combine_feature.shape[0], 1))))
+
+    #+0.001race_occupation
+    ttmp = np.core.defchararray.add(data[:,6].astype(np.str), "#")
+    combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,8]).reshape((combine_feature.shape[0], 1))))
+
+    #+0.0005race_native-country
+    #ttmp = np.core.defchararray.add(data[:,8].astype(np.str), "#")
+    #combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,13]).reshape((combine_feature.shape[0], 1))))
+
+    #=race_native-country_occupation
+    #ttmp = np.core.defchararray.add(data[:,6].astype(np.str), "#")
+    #ttmp = np.core.defchararray.add(data[:,8].astype(np.str), "#")
+    #combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,13]).reshape((combine_feature.shape[0], 1))))
+    
+    #+0.001workclass_country
+    ttmp = np.core.defchararray.add(data[:,1].astype(np.str), "#")
+    combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,13]).reshape((combine_feature.shape[0], 1))))
+    #+0.006education_hours-per-week
+    ttmp = np.core.defchararray.add(data[:,3].astype(np.str), "#")
+    combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,12]).reshape((combine_feature.shape[0], 1))))
+    #+0.013age_education
+    ttmp = np.core.defchararray.add(data[:,0].astype(np.str), "#")
+    combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,3]).reshape((combine_feature.shape[0], 1))))
+
+    #+0.01age_workclass
+    ttmp = np.core.defchararray.add(data[:,0].astype(np.str), "#")
+    combine_feature = np.column_stack((combine_feature, np.core.defchararray.add(ttmp, data[:,1]).reshape((combine_feature.shape[0], 1))))
+
+
+
+
+
+
+
 
     #组合特征单独处理
-    
     t = csr_matrix((combine_feature.shape[1], 1)) 
     combine_feature_result = None
     for col in range(combine_feature.shape[1]):
